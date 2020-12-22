d87f8742096bfe47734b5ace0350543dde05ae0a
Yau <ending1221@gmail.com>
2020-12-22 20:29:57 +0800

    [新增] 樹叢移動

--- js/main.js
+++ js/main.js
@@ -3,12 +3,22 @@ $(function () {
       var scrollVal = $(this).scrollTop();
       const $chi = $('.chi');
       const $grass = $('.grass');
-      
-      if(scrollVal >= 100){
-        $('.chi').addClass('scroll_100')
-      }else {
-        $('.chi').removeClass('scroll_100')
+
+      // $grass.css('bottom', `${-200 + scrollVal}px`)
+
+      if(scrollVal <= 300){
+        $grass.css('bottom', `${-300 + scrollVal}px`)
       }
+      if(scrollVal >= 600){
+        $grass.css('left', `${scrollVal-600}px`)
+      }
+
+      // if(scrollVal >= 100){
+      //   $grass.removeClass('down');
+      //   // $('.chi').addClass('scroll_100')
+      // }else {
+      //   // $('.chi').removeClass('scroll_100')
+      // }
       console.log(scrollVal)
     });
   });
